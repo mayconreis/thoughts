9cf77c4e97b8cbc9e0e3a449ce8248537b9509db
Maycon Reis <mayconreis221@gmail.com>
2022-08-31 11:55:56 -0300

    Retornando registros para a Dashboard

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -7,7 +7,23 @@ module.exports = class ToughtController {
     }
 
     static async dashboard(req, res) {
-        res.render('toughts/dashboard')
+
+        const userId = req.session.userId
+
+        const user = await User.findOne({
+            where: { id: userId },
+            include: Tought,
+            plain: true,
+        })
+
+        if (!user) {
+            res.redirect('/login')
+        }
+
+        const toughts = user.Toughts.map((result) => result.dataValues)
+        console.log(toughts)
+
+        res.render('toughts/dashboard', { toughts })
     }
 
     static createTought(req, res) {
@@ -15,16 +31,20 @@ module.exports = class ToughtController {
     }
 
     static async createToughtPost(req, res) {
+        const userId = req.session.userId
 
-        if(!req.session.userId){
+        const user = await User.findOne({ where: { id: userId } })
+
+        if (!user) {
             res.redirect('/login')
-        } else {
-            const tought = {
-                title: req.body.title,
-                UserId: req.session.userId
-            }
-    
-            await Tought.create(tought)
+        }
+
+        const tought = {
+            title: req.body.title,
+            UserId: req.session.userId
+        }
+
+        await Tought.create(tought)
             .then(() => {
                 req.session.save(() => {
                     req.flash('message', 'Tought create succesfully!')
@@ -32,7 +52,6 @@ module.exports = class ToughtController {
                 })
             })
             .catch((err) => console.log(err))
-        }
 
     }
 }
