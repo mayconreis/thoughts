3d3cbf885a49c372fa4d501bafe0bc979c16eb2b
Maycon Reis <mayconreis221@gmail.com>
2022-09-01 00:59:41 -0300

    Ajustando configurações para deploy no Heroku

--- db/conn.js
+++ db/conn.js
@@ -1,10 +1,16 @@
 const { Sequelize } = require('sequelize')
 require('dotenv').config()
 
-const sequelize = new Sequelize({
-    connectioString: process.env.DATABASE_URL
-})
-
+const sequelize = new Sequelize(
+    process.env.DATABASE_URL
+    // process.env.DB_NAME,
+    // process.env.DB_USER,
+    // process.env.DB_PASS,
+    // {
+    //     host: process.env.DB_HOST,
+    //     dialect: 'mysql'
+    // }
+)
 
 sequelize.authenticate()
     .then(() => {
